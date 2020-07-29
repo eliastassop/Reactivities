f0a522d52c3b3e097a96ee52ca684c24d8526f62
e.tassopoulos <e.tassopoulos@DESKTOP-OVU4AUE>
2020-07-29 16:12:28 +0300

    End of Section 7.

--- client-app/src/app/stores/activityStore.ts
+++ client-app/src/app/stores/activityStore.ts
@@ -1,7 +1,9 @@
-import { observable, action, computed } from "mobx";
+import { observable, action, computed, configure, runInAction } from "mobx";
 import { createContext, SyntheticEvent } from "react";
 import { IActivity } from "../models/activity";
 import agent from "../api/agent";
+
+configure({ enforceActions: "always" });
 
 class ActivityStore {
   @observable activityRegistry = new Map();
@@ -25,26 +27,36 @@ class ActivityStore {
 
     try {
       const activities = await agent.Activities.list();
-      activities.forEach((activity) => {
-        activity.date = activity.date.split(".")[0];
-        // split on . then access the first part of the returned array to remove the time accuracy
-        this.activityRegistry.set(activity.id, activity);
+      runInAction("loading activities", () => {
+        activities.forEach((activity) => {
+          activity.date = activity.date.split(".")[0];
+          // split on . then access the first part of the returned array to remove the time accuracy
+          this.activityRegistry.set(activity.id, activity);
+        });
+        this.loadingInitial = false;
       });
-      this.loadingInitial = false;
     } catch (error) {
+      runInAction("load activities error", () => {
+        this.loadingInitial = false;
+      });
+
       console.log(error);
-      this.loadingInitial = false;
     }
   };
   @action createActivity = async (activity: IActivity) => {
     this.submitting = true;
     try {
       await agent.Activities.create(activity);
-      this.activityRegistry.set(activity.id, activity);
-      this.editMode = false;
-      this.submitting = false;
+      runInAction("creating activity", () => {
+        this.activityRegistry.set(activity.id, activity);
+        this.editMode = false;
+        this.submitting = false;
+      });
     } catch (error) {
-      this.submitting = false;
+      runInAction("create activity error", () => {
+        this.submitting = false;
+      });
+
       console.log(error);
     }
   };
@@ -53,12 +65,16 @@ class ActivityStore {
     this.submitting = true;
     try {
       await agent.Activities.update(activity);
-      this.activityRegistry.set(activity.id, activity);
-      this.selectedActivity = activity;
-      this.editMode = false;
-      this.submitting = false;
+      runInAction("editing activity", () => {
+        this.activityRegistry.set(activity.id, activity);
+        this.selectedActivity = activity;
+        this.editMode = false;
+        this.submitting = false;
+      });
     } catch (error) {
-      this.submitting = false;
+      runInAction("editing activity error", () => {
+        this.submitting = false;
+      });
       console.log(error);
     }
   };
@@ -70,15 +86,20 @@ class ActivityStore {
     this.target = event.currentTarget.name;
     try {
       await agent.Activities.delete(id);
-      this.activityRegistry.delete(id);
-      this.submitting = false;
-      this.target = "";
+      runInAction("deleting activity", () => {
+        this.activityRegistry.delete(id);
+        this.submitting = false;
+        this.target = "";
 
-      // diko mou
-      this.selectedActivity = undefined;
+        // diko mou
+        this.selectedActivity = undefined;
+      });
     } catch (error) {
-      this.submitting = false;
-      this.target = "";
+      runInAction("deleting activity error", () => {
+        this.submitting = false;
+        this.target = "";
+      });
+
       console.log(error);
     }
   };
--- client-app/src/features/activities/form/ActivityForm.tsx
+++ client-app/src/features/activities/form/ActivityForm.tsx
@@ -2,25 +2,21 @@ import React, { useState, FormEvent, useContext } from "react";
 import { Segment, Form, Button } from "semantic-ui-react";
 import { IActivity } from "../../../app/models/activity";
 import { v4 as uuid } from "uuid";
-import ActivityStore from '../../../app/stores/activityStore'
+import ActivityStore from "../../../app/stores/activityStore";
+import { observer } from "mobx-react-lite";
 
 interface IProps {
-  
   activity: IActivity;
-  
-  
-  
 }
 
-const ActivityForm: React.FC<IProps> = ({
-  
-  activity: initialFormState,
-  
- 
- 
-}) => {
-  const activityStore = useContext(ActivityStore)
-  const {createActivity, editActivity, submitting, cancelFormOpen}= activityStore;
+const ActivityForm: React.FC<IProps> = ({ activity: initialFormState }) => {
+  const activityStore = useContext(ActivityStore);
+  const {
+    createActivity,
+    editActivity,
+    submitting,
+    cancelFormOpen,
+  } = activityStore;
   const iniitializeForm = () => {
     if (initialFormState) {
       return initialFormState;
@@ -119,4 +115,4 @@ const ActivityForm: React.FC<IProps> = ({
     </Segment>
   );
 };
-export default ActivityForm;
+export default observer(ActivityForm);
